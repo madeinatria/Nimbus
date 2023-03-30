5e27fc34170ad99469e840139e8e276cb1669742
Surya M <surya.m@phonepe.com>
2023-03-30 19:07:26 +0530

    added python base apis as docs

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -19,5 +19,19 @@ func setupApi(router *gin.Engine) {
 		api.GET("/", func(ctx *gin.Context) {
 			ctx.JSON(200, gin.H{"supported_apis": "supported_endpoints"})
 		})
+		/*	python base - apis
+			@app.get("/api/v1/user")
+			@app.post("/api/v1/user/signup")
+			@app.post("/api/v1/user/login")
+			@app.post("/api/v1/client/signup")
+			@app.get("/api/v1/client")
+			@app.get("/api/v1/card/levels")
+			@app.post("/api/v1/card/level/add")
+			@app.post("/api/v1/card/add")
+			@app.post("/api/v1/client/login")
+			@app.post("/api/v1/authorize")
+			@app.get("/api/v1/transaction")
+			@app.get('/api/v1/cards')
+		*/
 	}
 }
