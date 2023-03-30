c37c8ca09c4c472002bba64d3bdb8de758b9f42b
Surya M <surya.m@phonepe.com>
2023-03-30 19:05:21 +0530

    added gin

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,1 +1,23 @@
 package server
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	router := gin.New()
+	setupApi(router)
+
+	router.Run(fmt.Sprintf(":%s", "8081"))
+}
+
+func setupApi(router *gin.Engine) {
+	api := router.Group("/api/v1")
+	{
+		api.GET("/", func(ctx *gin.Context) {
+			ctx.JSON(200, gin.H{"supported_apis": "supported_endpoints"})
+		})
+	}
+}
