df84b04645c43e1cccf1f7f82f9fce49e963bf50
Surya M <surya.m@phonepe.com>
2023-03-30 19:43:54 +0530

    updated model

--- cmd/datastore/datastore.go
+++ cmd/datastore/datastore.go
@@ -1,49 +1,45 @@
 package datastore
 
 import (
-	"log"
-	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-
 type Client struct {
-	gorm.model
-	ID         uint   `gorm:"primaryKey"`
-	Name       string `json:"name"`
-	Phone      string `json:"phone"`
-	Details    string `json:"details"`
+	gorm.Model
+	ID      uint   `gorm:"primaryKey"`
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Details string `json:"details"`
 }
 
 type User struct {
-	gorm.model
-	ID         uint   `gorm:"primaryKey"`
-	Name       string `json:"name"`
-	Phone      string `json:"phone"`
-	Details    string `json:"details"`
+	gorm.Model
+	ID      uint   `gorm:"primaryKey"`
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Details string `json:"details"`
 }
 
 type Card string
 
 const (
-	Basic string = "Basic"
-	Premium      = "Premium"
-	Platinum     = "Platinum"
+	Basic    string = "Basic"
+	Premium         = "Premium"
+	Platinum        = "Platinum"
 )
 
 type Offers struct {
-	gorm.model
-	ID            uint   `gorm:"primaryKey"`
-	CardName      Card `json:"name"`
-	DiscountRate  uint `json:"phone"`
-	Client        Client `json:"client"`
+	gorm.Model
+	ID           uint   `gorm:"primaryKey"`
+	CardName     Card   `json:"name"`
+	DiscountRate uint   `json:"phone"`
+	Client       Client `json:"client"`
 }
 
 type Transaction struct {
-	gorm.model
-	ID            uint   `gorm:"primaryKey"`
-	Offer         Offers `json:"offer"`
-	Amount        uint `json:"amount"`
-	UserId        User `json:"user"`
+	gorm.Model
+	ID     uint   `gorm:"primaryKey"`
+	Offer  Offers `json:"offer"`
+	Amount uint   `json:"amount"`
+	UserId User   `json:"user"`
 }
-
